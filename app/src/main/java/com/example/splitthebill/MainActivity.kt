5e8af161cd10b9a92097332a0b880e11b3013c6e
Kate Rossinskaya <ekaterina.rossinskaia@r1.team>
2025-02-21 15:46:39 +0300

    add: добавила навигацию в приложение

--- app/src/main/java/com/example/splitthebill/MainActivity.kt
+++ app/src/main/java/com/example/splitthebill/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.splitthebill.ui.theme.SplitTheBillTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SplitTheBillTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SplitTheBillTheme {
-        Greeting("Android")
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "home") {
+        //composable("home") { HomeScreen(navController) }
+        //composable("details") { DetailsScreen(navController) }
     }
 }
